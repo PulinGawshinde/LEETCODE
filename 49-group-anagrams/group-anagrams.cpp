65ce5aff74736ca7e700b3ff882a792ed5e6262a
PulinGawshinde <pulingavshinde.4444@gmail.com>
2026-09-22 01:36:05 +0530

    Time: 9 ms (94.30%) | Memory: 23.7 MB (95.46%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,int> mp ;
+        vector<vector<string>> answer ; 
+        int row=0;
+        
+        for(int i = 0 ; i<strs.size(); i++){
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            if(mp.find(s)!= mp.end()){
+                answer[mp[s]].push_back(strs[i]);
+            }
+            else{
+                mp[s] = row;
+                answer.push_back({strs[i]});
+                row++;
+            }
+        }
+        return answer;
+    }
+};
