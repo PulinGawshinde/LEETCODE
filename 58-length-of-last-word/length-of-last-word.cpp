44306a7e561cfd29f18e09bc74936175e61ae790
PulinGawshinde <pulingavshinde.4444@gmail.com>
2026-09-21 21:26:17 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (32.88%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,7 +3,7 @@ public:
     int lengthOfLastWord(string s) {
         int i=s.size()-1;
         int count=0;
-        while(s[i]==' '){
+        while(s[i]==' '&&i>=0){
             i--;
         }
         for(i;i>=0;i--){
